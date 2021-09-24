3eebf5d824f1ea177751e86b48bf0502ccdfd955
Sora8d <miguelpx2012@gmail.com>
2021-09-23 23:55:47 -0300

    2

--- GoLang/tictact/tictact.go
+++ GoLang/tictact/tictact.go
@@ -30,6 +30,35 @@ func transformValuesToStrings(arr [3]int) [3]string {
 	return formatted_string
 }
 
+func checkMul(array [3][3]int, configuration [2][2]int) (int, bool) {
+	var n_in_pos [3]int
+	current_pos := array[configuration[0][0]][configuration[0][1]]
+	for i := 0; i < 3; i++ {
+		n_in_pos[i] = current_pos
+		configuration[0][0] += configuration[1][0]
+		configuration[0][1] += configuration[1][1]
+		current_pos = array[configuration[0][0]][configuration[0][1]]
+	}
+	if (n_in_pos[0] != 0) && (n_in_pos[0] == n_in_pos[1]) && (n_in_pos[0] == n_in_pos[2]) {
+		return n_in_pos[0], true
+	}
+	return 0, false
+}
+
+func (b *board) check() (int, bool) {
+	var win bool = false
+	var winner int
+	var config = [8][2][2]int{{{0, 0}, {0, 1}}, {{1, 0}, {0, 1}}, {{2, 0}, {0, 1}}, {{0, 0}, {1, 0}}, {{0, 1}, {1, 0}}, {{0, 2}, {1, 0}}, {{0, 0}, {1, 1}}, {{2, 0}, {-1, 1}}}
+	for i := 0; i < 8 && win == false; i++ {
+		winner, win = checkMul(b.board, config[i])
+		if win {
+			b.over = true
+			break
+		}
+	}
+	return winner, win
+}
+
 func (b *board) printBoard() {
 	for i := 0; i < 3; i++ {
 		to_print_values := transformValuesToStrings(b.board[i])
@@ -37,11 +66,13 @@ func (b *board) printBoard() {
 	}
 }
 
-func (b *board) move() {
-	fmt.Println("Please select the row and column of your movement in this format, row first then columns")
-	var rowString, colString string
-	fmt.Scanln(&rowString)
-	fmt.Scanln(&colString)
+func (b *board) move(test bool) {
+	if !test {
+		fmt.Println("Please select the row and column of your movement in this format, row first then columns")
+		var rowString, colString string
+		fmt.Scanln(&rowString)
+		fmt.Scanln(&colString)
+	}
 
 	var rowInt, colInt int
 	_, err := fmt.Sscan(rowString, &rowInt)
@@ -61,10 +92,10 @@ func (b *board) move() {
 
 }
 
-func (b *board) Build_game() {
+func (b *board) BuildGame() {
 	b.turn = 1
 }
-func (b *board) Start_game() {
+func (b *board) StartGame() {
 	b.printBoard()
 	for !b.over {
 		b.move()
@@ -73,6 +104,6 @@ func (b *board) Start_game() {
 }
 func main() {
 	var game1 board
-	game1.Build_game()
-	game1.Start_game()
+	game1.BuildGame()
+	game1.StartGame()
 }
